709332200d842d517d70c3b72bc29269b254afad
sphenry2799 <sp.henry2799@gmail.com>
2025-11-13 16:34:45 +0000

    Add Django to requirements

--- planorabookings/settings.py
+++ planorabookings/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,11 +79,16 @@ WSGI_APPLICATION = 'planorabookings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': 
+    dj_database_url.parse(os.environ.get("postgresql://neondb_owner:npg_A4s6WIqfYpob@ep-super-bonus-agl286rn.c-2.eu-central-1.aws.neon.tech/mower_food_outer_536113"))
 }
 
 
