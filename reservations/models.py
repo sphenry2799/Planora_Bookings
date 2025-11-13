8e47caa6e40235a09bdb2280253ce7201cbdab6b
sphenry2799 <sp.henry2799@gmail.com>
2025-11-13 15:06:32 +0000

    Add reservation model

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Reservation(models.Model):
+    """
+    Represents a reservation for a specific resource.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    date = models.DateField(default=timezone.now)
+    time = models.TimeField()
+    guests = models.PositiveIntegerField(default=1)
+    notes = models.TextField(blank=True)
+
+    class Meta:
+        unique_together = ('resource', 'date', 'time')
+        ordering = ['date', 'time']
+
+    def __str__(self):
+        return f'{self.name} - {self.date} {self.time} ({self.resource})'
