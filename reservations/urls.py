38f6b08dc769721bc48343fa7dfe4402b1066875
sphenry2799 <sp.henry2799@gmail.com>
2025-11-15 18:08:34 +0000

    Fix URLs and template issues for Heroku

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
 from django.urls import path
-from reservations import views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),  # home page → reservation form
+    path('reservations/', views.reservation_list, name='reservation_list'),
 ]
