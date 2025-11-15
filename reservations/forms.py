a99d217e195a54e7dfbf0067aad54c79c79fb423
sphenry2799 <sp.henry2799@gmail.com>
2025-11-15 17:54:33 +0000

    Amend code in forms.py

--- reservations/forms.py
+++ reservations/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from .models import Reservation
 
 class ReserveForm(forms.ModelForm):
-   class Meta:
-      model = Reservation
-      fields = '__all__'
+    class Meta:
+        model = Reservation
+        fields = '__all__'
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'time': forms.TimeInput(attrs={'type': 'time', 'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'party_size': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
