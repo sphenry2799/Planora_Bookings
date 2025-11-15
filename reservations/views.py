2ad57b88f4fdc0e99a96059e1dd8940d1ec3c502
sphenry2799 <sp.henry2799@gmail.com>
2025-11-15 17:55:01 +0000

    Amend redirect in views.py

--- reservations/views.py
+++ reservations/views.py
@@ -6,8 +6,9 @@ def home(request):
         form = ReserveForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')  # or a 'success' page if you prefer
+            return redirect('home')  # you could redirect to a success page
     else:
         form = ReserveForm()
     
     return render(request, 'reservation_home.html', {'form': form})
+
