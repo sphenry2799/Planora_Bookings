e9f523872101b8bdf54a80659899e170da518dd7
sphenry2799 <sp.henry2799@gmail.com>
2025-11-15 18:21:12 +0000

    Fix project-level urls to include app URLs

--- planorabookings/urls.py
+++ planorabookings/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include  # include allows app urls
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('reservations/', views.reservation_list, name='reservation_list'),
+    path('admin/', admin.site.urls),
+    path('', include('reservations.urls')),  # delegate all other routes to reservations app
 ]
