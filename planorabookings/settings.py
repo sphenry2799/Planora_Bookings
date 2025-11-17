faeba1af68fa772ac80c935dc289ab3b79c67a17
sphenry2799 <sp.henry2799@gmail.com>
2025-11-17 15:15:46 +0000

    enable serving of static files

--- planorabookings/settings.py
+++ planorabookings/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # STATIC FILES
 # -------------------
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # -------------------
