5787aee0eb14671b1083003745a38492fa8510a8
sphenry2799 <sp.henry2799@gmail.com>
2025-11-18 17:25:41 +0000

    Amend settings for Heroku

--- planorabookings/settings.py
+++ planorabookings/settings.py
@@ -1,28 +1,27 @@
 import os
 from pathlib import Path
-import dj_database_url
 
-# Base directory
+# -------------------
+# BASE DIR
+# -------------------
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # -------------------
 # SECRET KEY
 # -------------------
-SECRET_KEY = os.environ.get("SECRET_KEY")  
+SECRET_KEY = os.environ.get("SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY not set in environment variables!")
 
 # -------------------
 # DEBUG
 # -------------------
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # -------------------
 # ALLOWED HOSTS
 # -------------------
-if DEBUG:
-    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-else:
-    ALLOWED_HOSTS = ['planora-reservations-32b582e35d5e.herokuapp.com']
+ALLOWED_HOSTS = ['planora-reservations-32b582e35d5e.herokuapp.com']
 
 # -------------------
 # INSTALLED APPS
@@ -35,123 +34,37 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+
+    # django-allauth
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'reservations', 
+
+    # your apps
+    'reservations',
     'django_summernote',
     'about',
 ]
 
+# -------------------
+# Sites framework
+# -------------------
 SITE_ID = 1
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
 
 # -------------------
 # MIDDLEWARE
 # -------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # if using whitenoise
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # for static files on Heroku
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',  # must be here
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
 ]
 
 # -------------------
-# ROOT URLS
-# -------------------
-ROOT_URLCONF = 'planorabookings.urls'
-
-# -------------------
-# TEMPLATES
-# -------------------
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-
-# -------------------
-# WSGI
-# -------------------
-WSGI_APPLICATION = 'planorabookings.wsgi.application'
-
-# -------------------
-# DATABASE
-# -------------------
-DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
-    )
-}
-
-# -------------------
-# PASSWORD VALIDATION
-# -------------------
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-# -------------------
-# Allauth settings
-# -------------------
-ACCOUNT_EMAIL_VERIFICATION = 'none'
-ACCOUNT_AUTHENTICATION_METHOD = 'username'
-ACCOUNT_EMAIL_REQUIRED = False
-
-
-
-# -------------------
-# INTERNATIONALIZATION
-# -------------------
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_TZ = True
-
-# -------------------
-# STATIC FILES
-# -------------------
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# -------------------
-# DEFAULT AUTO FIELD
-# -------------------
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# ROOT URL
+# ------------------
