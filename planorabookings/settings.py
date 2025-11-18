63f54c57d095f4b16c821c9e2a6d079111625247
sphenry2799 <sp.henry2799@gmail.com>
2025-11-18 17:21:02 +0000

    Add email settings

--- planorabookings/settings.py
+++ planorabookings/settings.py
@@ -119,7 +119,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# -------------------
+# Allauth settings
+# -------------------
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_AUTHENTICATION_METHOD = 'username'
+ACCOUNT_EMAIL_REQUIRED = False
+
 
 
 # -------------------
