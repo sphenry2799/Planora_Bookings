70c92d16b86afe8ad3b36391460934a65fdef0cc
sphenry2799 <sp.henry2799@gmail.com>
2025-11-18 15:55:49 +0000

    Install django summernote and add settings

--- planorabookings/settings.py
+++ planorabookings/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'reservations', 
+    'django_summernote',
     'about',
 ]
 
--- planorabookings/urls.py
+++ planorabookings/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include  # include allows app urls
 urlpatterns = [
     path("about/", include("about.urls"), name="about-urls"),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('', include('reservations.urls')),  
     path("accounts/", include("allauth.urls")),
 ]
