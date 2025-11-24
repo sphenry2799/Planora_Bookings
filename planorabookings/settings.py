671e2a00fc6a316a0cafb3332bea1e3798ac6d83
sphenry2799 <sp.henry2799@gmail.com>
2025-11-24 12:07:18 +0000

    Update secret key settings

--- planorabookings/settings.py
+++ planorabookings/settings.py
@@ -10,9 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -------------------
 # SECRET KEY
 # -------------------
-SECRET_KEY = os.environ.get("SECRET_KEY")
-if not SECRET_KEY:
-    raise Exception("SECRET_KEY not set in environment variables!")
+print("SECRET_KEY:", os.environ.get("SECRET_KEY"))
+
 
 # -------------------
 # DEBUG
