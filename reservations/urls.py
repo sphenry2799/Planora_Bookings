2a777cea7e9f655a74a3c4aacab8a9913a43eda9
sphenry2799 <sp.henry2799@gmail.com>
2025-12-02 19:01:35 +0000

    Add paths to urls.py

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Home / reservation form
     path("reservation_confirmation/", views.reservation_confirmation, name="reservation_confirmation"),
     path('list/', views.reservation_list, name='reservation_list'),
     path('edit/<int:pk>/', views.edit_reservation, name='edit_reservation'),
     path('delete/<int:pk>/', views.delete_reservation, name='delete_reservation'),
+    path('login/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path('reserve/', views.make_reservation, name='make_reservation'),
 ]
-
